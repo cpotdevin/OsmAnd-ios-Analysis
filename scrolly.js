d33c2164953354679e62b78e460f830331e31d5f
cpotdevin <chrpotdevin@gmail.com>
2019-11-20 12:58:08 -0500

    feat: add scrollytelling

---
+++ scrolly.js
@@ -0,0 +1,53 @@
+console.log('blablabla')
+
+const scroller = scrollama();
+
+const main = d3.select('main');
+const scrollys = main.selectAll('.scrolly');
+const articles = scrollys.selectAll('article');
+const steps = articles.selectAll('.step');
+const figures = scrollys.selectAll('figure');
+
+function handleResize() {
+    // const stepH = Math.floor(window.innerHeight * 0.75);
+    // step.style('height', stepH + 'px');
+    const figureHeight = window.innerHeight * 4 / 5;
+    const figureMarginTop = (window.innerHeight - figureHeight) / 2;
+    figures
+        .style('height', figureHeight + 'px')
+        .style('top', figureMarginTop + 'px');
+    scroller.resize();
+}
+
+function handleStepEnter(response) {
+    console.log(response);
+
+    steps.classed('is-active', function(d, i) {
+        return i === Number(response.element.getAttribute('data-step'));
+    })
+
+    const scrollerId = response.element.classList[1];
+    scrollys.select('figure.' + scrollerId)
+        .select('img')
+        .attr('src', response.element.getAttribute('img-src'));
+}
+
+function setupStickyfill() {
+    d3.selectAll('.sticky').each(function () {
+        Stickyfill.add(this);
+    });
+}
+
+function init() {
+    setupStickyfill();
+    handleResize();
+    scroller.setup({
+        step: '.scrolly article .step',
+        offset: 0.33,
+        debug: false,
+    }).onStepEnter(handleStepEnter);
+
+    window.addEventListener('resize', handleResize);
+}
+
+init();
